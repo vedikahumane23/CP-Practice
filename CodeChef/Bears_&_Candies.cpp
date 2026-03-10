8b63200efd08693f2d5c531a4588fc180b4699bf
unknown <vedikahumane34@gmail.com>
2026-03-10 14:38:45 +0530

    Solved Bear and Candies 123

---
+++ CodeChef/Bears_&_Candies.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int tc;
+	cin >> tc;
+	while(tc--){
+	    int a, b;
+	    cin >> a >> b;
+	   int turn = 1;
+	   while(true){
+	       if(turn%2){
+	           if(a<turn){cout << "bob\n"; break;}
+	           a-=turn;
+	       }else{
+	           if(b<turn){cout << "limak\n";break;}
+	           b-=turn;
+	       }
+	        turn++;
+	   }
+	}
+}
